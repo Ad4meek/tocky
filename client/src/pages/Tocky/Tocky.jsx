ab407d712c972aef7eaa1ec87967f6ef52758018
pavel-skala <pavel.skala@student.spsmb.cz>
2024-06-03 13:01:13 +0200

    tocky

--- client/src/pages/Tocky/Tocky.jsx
+++ client/src/pages/Tocky/Tocky.jsx
@@ -1,5 +1,7 @@
 import "./Tocky.css";
 import AutorenewIcon from "@mui/icons-material/Autorenew";
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
 
 export default function Tocky() {
     const repeat = () => {
@@ -8,7 +10,18 @@ export default function Tocky() {
 
     return (
         <>
-            <p>tocky</p>
+            <h1>tocky</h1>
+            <div className="tocka"></div>
+            <p>hodnota sazky</p>
+            <div>
+              <button>
+                <RemoveIcon></RemoveIcon>
+              </button>
+              <p></p>
+              <button>
+                <AddIcon></AddIcon>
+              </button>
+            </div>
             <button onClick={repeat}>
                 <AutorenewIcon></AutorenewIcon>
             </button>
