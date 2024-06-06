5ac082d346947e0d6c9149b698dd16916ed41651
pavel-skala <pavelskalamobil@gmail.co>
2024-06-06 22:47:55 +0200

    tocky

--- client/src/pages/Tocky/Tocky.jsx
+++ client/src/pages/Tocky/Tocky.jsx
@@ -9,46 +9,137 @@ export default function Tocky() {
     const [spinValue, setSpinValue] = useState(1);
     const [spinValueIndex, setSpinValueIndex] = useState(0);
     const spinValuesConstant = [1, 2, 5, 10, 20, 50, 100, 200, 500, 1000];
-    const [backgroundPosition, setBackgroundPosition] = useState(Array(9).fill("0px 0px"));
+    const [backgroundPosition, setBackgroundPosition] = useState(Array(16).fill("0px 0px"));
     const [spinnedTypes, setSpinnedTypes] = useState([]);
 
+    const spinTypesConstant = Object.keys(spinTypes);
+
+    const acrossIndexes = [
+        [0, 5, 10],
+        [1, 6, 11],
+        [2, 5, 8],
+        [3, 6, 9],
+        [4, 9, 14],
+        [5, 10, 15],
+        [6, 9, 12],
+        [7, 10, 13],
+    ];
     // console.log(backgroundPosition);
 
     const repeat = () => {
         console.log("točka", spinValue);
 
-        const newBackgroundPositions = [];
-        const newSpinnedTypes = [];
+        let newBackgroundPositions = [];
+        let newSpinnedTypes = [];
         for (let i = 0; i < backgroundPosition.length; i++) {
-            const num = randomNumber(spinTypesArray.length);
-            const spinType = spinTypesArray[num];
+            const num = randomNumber(spinTypesConstant.length);
+            const spinType = spinTypesConstant[num];
             const newPosition = spinTypes[spinType].position;
 
-            newSpinnedTypes.push(spinType);
+            if (i % 4 === 0) newSpinnedTypes.push([]);
+            newSpinnedTypes[newSpinnedTypes.length - 1].push(spinType);
+
             newBackgroundPositions.push(`-${newPosition.x}px -${newPosition.y}px`);
         }
         setSpinnedTypes(newSpinnedTypes);
         setBackgroundPosition(newBackgroundPositions);
 
         console.log(newSpinnedTypes);
-        const winningTypes = [];
-        const winStatus = newSpinnedTypes.some((type) => {
-            console.log(winningTypes.some(wType => wType.type == type))
-            if (winningTypes.length === 0 || winningTypes.some((wType) => wType.type !== type)) {
-                const typeCount = newSpinnedTypes.filter((t) => t === type).length;
-                console.log(type, typeCount);
-                if (typeCount >= 3) {
-                    winningTypes.push({ type, count: typeCount });
+        checkWin(newSpinnedTypes);
+
+        // if (winStatus) {
+        //     win();
+        // }
+    };
+
+    const checkWin = (spinnedTypes) => {
+        console.log("checkWin");
+
+        let winningItems = [];
+
+        // check horizontal
+        spinnedTypes.forEach((spinnedTypesRow, rowIndex) => {
+            spinTypesConstant.forEach((spinType) => {
+                const count = spinnedTypesRow.filter((type) => type === spinType).length;
+                // console.log(count, spinType);
+                if (count >= 3) {
+                    const indexes = getIndexes(spinnedTypesRow, rowIndex, spinType);
+
+                    // are consecutive
+                    if (isConsecutive(indexes)) {
+                        let itemsArray = [];
+                        indexes.forEach((index) => {
+                            const item = {
+                                index,
+                                type: spinType,
+                            };
+                            itemsArray.push(item);
+                        });
+
+                        winningItems.push(itemsArray);
+                        console.log("winningItems", winningItems);
+                    }
                 }
+            });
+        });
+
+        // check across
+        acrossIndexes.forEach((acrossIndex) => {
+            let types = [];
+
+            acrossIndex.forEach((index) => {
+                const rowIndex = Math.floor(index / 4);
+                const columnIndex = index % 4;
+                types.push(spinnedTypes[rowIndex][columnIndex]);
+            });
+
+            if (types.every((type, _, arr) => type === arr[0])) {
+                console.log("win across");
+
+                let itemsArray = [];
+                acrossIndex.forEach((index) => {
+                    const item = {
+                        index,
+                        type: types[0],
+                    };
+                    itemsArray.push(item);
+                });
+
+                winningItems.push(itemsArray);
+
+                console.log("winningItems", winningItems);
             }
         });
-        console.log(winningTypes);
-        if (winStatus) {
-            win();
+
+        if (winningItems.length > 0) {
+            win(winningItems);
         }
     };
 
-    const win = () => {
+    const getIndexes = (row, rowIndex, type) => {
+        let indexes = [];
+
+        row.forEach((item, index) => {
+            if (item === type) {
+                indexes.push(index + rowIndex * 4);
+            }
+        });
+        return indexes;
+    };
+
+    const isConsecutive = (indexes) => {
+        let array = [];
+        indexes.forEach((index) => {
+            array.push(index % 4);
+        });
+
+        if (array.includes(1) && array.includes(2)) {
+            return true;
+        }
+        return false;
+    };
+
+    const win = (winningItems) => {
         console.log("win");
     };
 
@@ -68,8 +159,6 @@ export default function Tocky() {
         setSpinValueIndex(spinValueIndex - 1);
     };
 
-    const spinTypesArray = Object.keys(spinTypes);
-
     const randomNumber = (max) => {
         return Math.floor(Math.random() * max);
     };
@@ -87,15 +176,6 @@ export default function Tocky() {
                         ></div>
                     );
                 })}
-                {/* <div className="tocka" style={{ backgroundPosition: backgroundPosition }}></div>
-                <div className="tocka" style={{ backgroundPosition: backgroundPosition }}></div>
-                <div className="tocka" style={{ backgroundPosition: backgroundPosition }}></div>
-                <div className="tocka" style={{ backgroundPosition: backgroundPosition }}></div>
-                <div className="tocka" style={{ backgroundPosition: backgroundPosition }}></div>
-                <div className="tocka" style={{ backgroundPosition: backgroundPosition }}></div>
-                <div className="tocka" style={{ backgroundPosition: backgroundPosition }}></div>
-                <div className="tocka" style={{ backgroundPosition: backgroundPosition }}></div>
-                <div className="tocka" style={{ backgroundPosition: backgroundPosition }}></div> */}
             </div>
             <div className="spinValue">
                 <button onClick={remove}>
