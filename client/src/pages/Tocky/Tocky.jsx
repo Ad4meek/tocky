ce0aadca919e3c10b28c2b89a03212cd9efae415
pavel-skala <pavelskalamobil@gmail.co>
2024-06-09 22:44:51 +0200

    tocky

--- client/src/pages/Tocky/Tocky.jsx
+++ client/src/pages/Tocky/Tocky.jsx
@@ -2,7 +2,7 @@ import "./Tocky.css";
 import AutorenewIcon from "@mui/icons-material/Autorenew";
 import AddIcon from "@mui/icons-material/Add";
 import RemoveIcon from "@mui/icons-material/Remove";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import spinTypes from "../../assets/spinTypes.json";
 import { Button } from "@mui/material";
 import { Link, Navigate, useNavigate } from "react-router-dom";
@@ -15,10 +15,8 @@ export default function Tocky() {
     const [spinValue, setSpinValue] = useState(1);
     const [spinValueIndex, setSpinValueIndex] = useState(0);
     const spinValuesConstant = [1, 2, 5, 10, 20, 50, 100, 200, 500, 1000];
-    const [backgroundPosition, setBackgroundPosition] = useState(
-        Array(16).fill("0px 0px")
-    );
-    const [spinnedTypes, setSpinnedTypes] = useState([]);
+    const [backgroundPosition, setBackgroundPosition] = useState(Array(16).fill("0px 0px"));
+    const [winValue, setWinValue] = useState(null);
 
     const spinTypesConstant = Object.keys(spinTypes);
 
@@ -32,10 +30,42 @@ export default function Tocky() {
         [6, 9, 12],
         [7, 10, 13],
     ];
-    // console.log(backgroundPosition);
+
+    const draw = (ctx, posX, posY, finalX, finalY, width, direction) => {
+        ctx.fillStyle = "red";
+        ctx.beginPath();
+        ctx.moveTo(posX, posY);
+        ctx.lineTo(finalX, finalY);
+        if (direction === "horizontal") {
+            ctx.lineTo(finalX, finalY + width);
+            ctx.lineTo(posX, posY + width);
+        } else {
+            ctx.lineTo(finalX + width, finalY);
+            ctx.lineTo(posX + width, posY);
+        }
+        ctx.lineTo(posX, posY);
+        ctx.fill();
+    };
+    const clearCanvas = (ctx) => {
+        ctx.clearRect(0, 0, 800, 800);
+    };
+
+    const canvasRef = useRef(null);
+    const [ctx, setCtx] = useState(null);
+
+    useEffect(() => {
+        const canvas = canvasRef.current;
+        canvas.width = 800;
+        canvas.height = 800;
+        setCtx(canvas.getContext("2d"));
+
+        // draw(ctx, 250, 650, 450, 650, 10);
+    }, []);
 
     const repeat = () => {
         console.log("točka", spinValue);
+        clearCanvas(ctx);
+        setWinValue(null);
 
         let newBackgroundPositions = [];
         let newSpinnedTypes = [];
@@ -47,11 +77,8 @@ export default function Tocky() {
             if (i % 4 === 0) newSpinnedTypes.push([]);
             newSpinnedTypes[newSpinnedTypes.length - 1].push(spinType);
 
-            newBackgroundPositions.push(
-                `-${newPosition.x}px -${newPosition.y}px`
-            );
+            newBackgroundPositions.push(`-${newPosition.x}px -${newPosition.y}px`);
         }
-        setSpinnedTypes(newSpinnedTypes);
         setBackgroundPosition(newBackgroundPositions);
 
         console.log(newSpinnedTypes);
@@ -70,26 +97,23 @@ export default function Tocky() {
         // check horizontal
         spinnedTypes.forEach((spinnedTypesRow, rowIndex) => {
             spinTypesConstant.forEach((spinType) => {
-                const count = spinnedTypesRow.filter(
-                    (type) => type === spinType
-                ).length;
+                const count = spinnedTypesRow.filter((type) => type === spinType).length;
                 // console.log(count, spinType);
                 if (count >= 3) {
-                    const indexes = getIndexes(
-                        spinnedTypesRow,
-                        rowIndex,
-                        spinType
-                    );
+                    const indexes = getIndexes(spinnedTypesRow, rowIndex, spinType);
 
                     // are consecutive
                     if (isConsecutive(indexes)) {
-                        let itemsArray = [];
+                        let itemsArray = {
+                            direction: "horizontal",
+                            items: [],
+                        };
                         indexes.forEach((index) => {
                             const item = {
                                 index,
                                 type: spinType,
                             };
-                            itemsArray.push(item);
+                            itemsArray.items.push(item);
                         });
 
                         winningItems.push(itemsArray);
@@ -112,13 +136,16 @@ export default function Tocky() {
             if (types.every((type, _, arr) => type === arr[0])) {
                 console.log("win across");
 
-                let itemsArray = [];
+                let itemsArray = {
+                    direction: "across",
+                    items: [],
+                };
                 acrossIndex.forEach((index) => {
                     const item = {
                         index,
                         type: types[0],
                     };
-                    itemsArray.push(item);
+                    itemsArray.items.push(item);
                 });
 
                 winningItems.push(itemsArray);
@@ -157,6 +184,55 @@ export default function Tocky() {
 
     const win = (winningItems) => {
         console.log("win");
+
+        let newWinValue = 0;
+
+        winningItems.forEach((item) => {
+            let items = item.items;
+            for (let i = 1; i < items.length; i++) {
+                const item1 = items[i - 1];
+                const item2 = items[i];
+                const offset = {
+                    horizontal: {
+                        x: 100,
+                        y: 110,
+                    },
+                    across: {
+                        x: 75,
+                        y: 100,
+                    },
+                };
+
+                const position1 = getPosition(item1.index, offset[item.direction]);
+                const position2 = getPosition(item2.index, offset[item.direction]);
+                console.log("position1", position1);
+                console.log("position2", position2);
+
+                let direction = "across";
+                let width = 20;
+                if (position1.y === position2.y) {
+                    direction = "horizontal";
+                    width = 12;
+                }
+
+                draw(ctx, position1.x, position1.y, position2.x, position2.y, width, direction);
+            }
+            console.log(item);
+
+            if (item.direction === "horizontal" && item.items.length === 4) {
+                newWinValue += 2 * spinTypes[item.items[0].type].win;
+            } else {
+                newWinValue += spinTypes[item.items[0].type].win;
+            }
+        });
+        setWinValue(newWinValue);
+    };
+
+    const getPosition = (index, offset) => {
+        return {
+            x: (index % 4) * 200 + offset.x,
+            y: Math.floor(index / 4) * 200 + offset.y,
+        };
     };
 
     const add = () => {
@@ -184,48 +260,46 @@ export default function Tocky() {
     const navigate = useNavigate();
 
     const [money, setMoney] = useState(0);
-    
-    
-    async function logout(){
+
+    async function logout() {
         await logoutUser();
-    
+
         dispatch(reset());
 
-        navigate('/');
+        navigate("/");
     }
 
     // Get money
     const [updateMoney, setUpdateMoney] = useState(false);
-    
+
     const userState = useSelector((state) => state.user);
-    
-    useEffect(() => {
-        async function asyncLoad(){
-            const userMoney = await getUserMoney(
-                userState.user.uniqueId
-            );
 
-            setMoney(userMoney.data.money);
-        }
+    // useEffect(() => {
+    //     async function asyncLoad(){
+    //         const userMoney = await getUserMoney(
+    //             userState.user.uniqueId
+    //         );
 
-        asyncLoad();
+    //         setMoney(userMoney.data.money);
+    //     }
 
-        if(updateMoney) setUpdateMoney(false);
-    }, [updateMoney])
+    //     asyncLoad();
+
+    //     if(updateMoney) setUpdateMoney(false);
+    // }, [updateMoney])
 
     // Add and remove money
-    async function removeMoney(amount){
+    async function removeMoney(amount) {
         await removeAmountMoney(amount, userState.user.uniqueId);
 
         setUpdateMoney(true);
     }
 
-    async function addMoney(amount){
+    async function addMoney(amount) {
         await addAmountMoney(amount, userState.user.uniqueId);
 
         setUpdateMoney(true);
     }
-
 
     return (
         <>
@@ -241,6 +315,7 @@ export default function Tocky() {
                     );
                 })}
             </div>
+            <canvas className="canvas" ref={canvasRef}></canvas>
             <div className="spinValue">
                 <button onClick={remove}>
                     <RemoveIcon></RemoveIcon>
@@ -249,11 +324,11 @@ export default function Tocky() {
                 <button onClick={add}>
                     <AddIcon></AddIcon>
                 </button>
+                <p>{winValue}</p>
             </div>
             <button onClick={repeat}>
                 <AutorenewIcon></AutorenewIcon>
             </button>
-
 
             <div className="bar">
                 <Button variant="contained" onClick={logout}>
@@ -261,18 +336,14 @@ export default function Tocky() {
                 </Button>
 
                 <Link to={"/deposit"}>
-                    <Button>
-                        Dej peníz
-                    </Button>
+                    <Button>Dej peníz</Button>
                 </Link>
 
                 <p>
                     Počet peněz:
-
-                    { money }
+                    {money}
                 </p>
             </div>
-
 
             <div>
                 <br />
@@ -281,13 +352,9 @@ export default function Tocky() {
                 <br />
                 <h3>Testing</h3>
 
-                <Button onClick={() => removeMoney(50)}>
-                    -50kč
-                </Button>
+                <Button onClick={() => removeMoney(50)}>-50kč</Button>
 
-                <Button onClick={() => addMoney(50)}>
-                    +50kč
-                </Button>
+                <Button onClick={() => addMoney(50)}>+50kč</Button>
             </div>
         </>
     );
